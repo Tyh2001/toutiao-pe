7d5d42904e1207b443fe810fa27a9f16dc9a2b33
Tyh2001 <Tyh2001@users.noreply.github.com>
2021-02-27 04:42:31 +0000

    Automated deployment: Sat Feb 27 04:42:31 UTC 2021 1a9755caaa673b15f9164c44a5fa9c7f041677c0

---
+++ js/chunk-2d0a2e01.b513cd4a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0a2e01"],{"007b":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("van-nav-bar",{attrs:{title:"视频"}}),t("div",[e._v("视频 - 开发中")])],1)},c=[],i={name:"videoIndex"},r=i,s=t("2877"),u=Object(s["a"])(r,a,c,!1,null,"40f5c248",null);n["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d0a2e01.b513cd4a.js.map
